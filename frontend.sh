75836ea6664fb5722c3a601d2c5871d285067a74
lohitha <gorlelohitha@gmail.com>
2023-07-19 13:27:37 +0530

    frontend script

--- frontend.sh
+++
@@ -1,7 +0,0 @@
-yum install nginx -y
-
-systemctl enable nginx
-systemctl start nginx
-
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
