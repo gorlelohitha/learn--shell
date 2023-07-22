2f500887fc7121463d12ee9c90ac9ed845454a57
lohitha <gorlelohitha@gmail.com>
2023-07-22 18:33:16 +0530

    Roboshop scripting update DNS Record

--- print.sh
+++ print.sh
@@ -1,1 +1,17 @@
 echo hello world
+
+#Red    31
+
+#Green   32
+#Yellow  33
+#Blue    34
+#Magenta  35
+#Cyan     36
+
+
+echo -e "e[31mHello word\e[om"
+echo -e "e[32mHello word\e[om"
+echo -e "e[33mHello word\e[om"
+echo -e "e[34mHello word\e[om"
+echo -e "e[35mHello word\e[om"
+echo -e "e[36mHello word\e[om"
